e3e709bda59c10cadb51ec5b53a9ea343aed2f8b
Kiis <karkiis@tktk.ee>
2024-02-17 23:35:13 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+//rakendus küsib kasutal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolataion
+Console.WriteLine($"Hello, {userName} !"); //Output
